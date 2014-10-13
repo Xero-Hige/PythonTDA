ff354dd29e34e69f2bd8ab2f676e4c246679528e
LMiguenz <leandro.v.059@gmail.com>
2014-10-13 13:50:13 -0300

    Corrijo errores y completo el módulo recommender.py. Modo de uso: invocando a la función show_recommendations(rutaDelGdf) se muestra por pantalla la lista de recomendaciones con los nombres de los contactos.

--- graph.py
+++ graph.py
@@ -106,3 +106,6 @@ class Graph(object):
 
 	def get_vertex_data(self,vertex_id):
 		return self.vertex[vertex_id].data
+
+	def get_all_vertices_ids(self):
+		return self.vertex.keys();
--- recommender.py
+++ recommender.py
@@ -1,68 +1,80 @@
 from facebook_graph import parse_file
 
-FACEBOOK_GRAPH_FILE = "./network.gdf"
-
 class _Recommendation(object):
 	""" Representa una recomendacion como un par
 	'id' (el usuario recomendado) y 'value' (la 
 	cantidad de amigos en comun con ese usuario)"""
 
-	def __init__(self):
-		self.recommendation_id = None
-		self.recommendation_value = None
+	def __init__(self, receiver):
+		self.receiver = receiver
+		self.id = None
+		self.value = None
 
 	def update(self, candidate_id, candidate_value):
 		"""Recibe un id y un valor, y si el valor recibido es
 		mayor que el valor de la recomendacion que se tiene 
 		hasta el momento, la reemplaza por la nueva"""
-		if(self.recommendation_value is None 
-			or candidate_value > self.recommendation_value):
-			self.recommendation_id = candidate_id
-			self.recommendation_value = candidate_value
-
-	def get_id(self):
-		return self.recommendation_id
-
-	def get_value(self):
-		return self.recommendation_value
+		if(self.value is None 
+			or candidate_value > self.value):
+			self.id = candidate_id
+			self.value = candidate_value
 
 
-def get_recommendation_for(contact_id):
+
+def get_recommendation_for(contact_id, fb_graph):
 	""" Devuelve una recomendacion de amistad para 
 	el contacto indicado, basada en la cantidad
 	de amigos en comun con dicho contacto """
 
-	fb_graph = parse_file(FACEBOOK_GRAPH_FILE)
 	#BFS capa 1:
 	fb_graph.mark_vertex(contact_id)
 	friends = fb_graph.get_vertex_neighbours(contact_id)
 	map(fb_graph.mark_vertex, friends)
-	names = []
 
 	#BFS capa 2;
 	candidates_for_recommendation = []
 	for friend in friends:
 		candidates_for_recommendation.extend(fb_graph.get_unmarked_vertex_neighbours(friend))
 
-	#CODIGO PARA PRUEBAS, BORRAR DESPUES
-	'''for asd in candidates_for_recommendation:
-		print(asd)
-		names.append(fb_graph.get_vertex_data(asd))
-	print(names)'''
 
-	recommendation = _Recommendation()
+	recommendation = _Recommendation(contact_id)
 	for candidate in candidates_for_recommendation:
 		value = len( fb_graph.get_marked_vertex_neighbours(candidate) )
 		recommendation.update(candidate, value)
-
+		
+	fb_graph.unmark_all()
 	return recommendation
 
 
+def get_recommendations(graph_filepath):
+	"""Devuelve una lista con una recomendacion por cada contacto
+	del archivo"""
+
+	fb_graph = parse_file(graph_filepath)
+	contacts = fb_graph.get_all_vertices_ids()
+
+	recommendations = []
+	for contact in contacts:
+		recommendations.append( get_recommendation_for(contact, fb_graph) )
+
+	return recommendations
+
+
+def show_recommendations(graph_filepath):
+	"""Funcion para presentar los resultados de las
+	recomendaciones por pantalla"""
+
+	recommendations = get_recommendations(graph_filepath)
+	fb_graph = parse_file(graph_filepath)
+
+	for recommendation in recommendations:
+		print fb_graph.get_vertex_data( recommendation.receiver ) + ": ",
+		if(recommendation.id is None):
+			print "no hay recomendaciones"
+		else:
+			print fb_graph.get_vertex_data( recommendation.id ),
+			print "(" + str(recommendation.value) + " amigos en comun)"
+
+
 #EJEMPLO DE USO
-recom = get_recommendation_for("1140884962")
-if(recom.get_id() is None):
-	print("No se encontraron recomendaciones")
-else:
-	g = parse_file(FACEBOOK_GRAPH_FILE)
-	print "Recomendacion: " + g.get_vertex_data( recom.get_id() )
-	print "Amigos en comun: " + str(recom.get_value())
+show_recommendations("./network.gdf")
